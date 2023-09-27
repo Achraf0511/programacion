a7ce39bdd279dbc8d967ef9b08bf676b55f10e6a
achnaj <lahsennajaichraf@gmail.com>
2023-09-27 12:37:40 +0200

    ejrcicios

--- Tema0/Tema1Ejercicio2.java
+++ Tema0/Tema1Ejercicio2.java
@@ -6,23 +6,11 @@ public class Tema1Ejercicio2 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Introduce el valor de la base del triángulo");
+        System.out.println("Introduce el valor de la base del rectángulo");
         int a = in.nextInt();
-        System.out.println ("Introduce el valor de la altura del triángulo");
-        int b= in.nextInt();
+        System.out.println("Introduce el valor de la altura del rectángulo");
+        int b = in.nextInt();
         System.out.println("Calculando área");
-        System.out.println((a * b)/2 + " ÁREA");
-        System.out.print("Introduce el valor de un lado del triángulo");
-        int c= in.nextInt();
-        System.out.println("Introduce el valor del otro lado del triángulo");
-        int d= in.nextInt();
-        System.out.println("Calculando el perímetro");
-        System.out.println(a + c + d + "Perímtero");
+        System.out.println(a * b + " Área");
     }
-
 }
-
-
-
-
-
